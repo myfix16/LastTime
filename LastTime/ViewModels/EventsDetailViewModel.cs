31ebf2d370ec88349bf3dbd214e6c6ba7aa83ebe
myfix16 <myfix16@outlook.com>
2020-11-05 17:12:40 +0800

    edit navigation view

---
+++ LastTime/ViewModels/EventsDetailViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+using LastTime.Core.Models;
+using LastTime.Core.Services;
+using LastTime.Helpers;
+
+namespace LastTime.ViewModels
+{
+    public class EventsDetailViewModel : Observable
+    {
+        private SampleOrder _item;
+
+        public SampleOrder Item
+        {
+            get { return _item; }
+            set { Set(ref _item, value); }
+        }
+
+        public EventsDetailViewModel()
+        {
+        }
+
+        public async Task InitializeAsync(long orderID)
+        {
+            var data = await SampleDataService.GetContentGridDataAsync();
+            Item = data.First(i => i.OrderID == orderID);
+        }
+    }
+}
---
+++ LastTime/Views/EventsDetailPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using LastTime.Services;
+using LastTime.ViewModels;
+
+using Microsoft.Toolkit.Uwp.UI.Animations;
+
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace LastTime.Views
+{
+    public sealed partial class EventsDetailPage : Page
+    {
+        public EventsDetailViewModel ViewModel { get; } = new EventsDetailViewModel();
+
+        public EventsDetailPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            this.RegisterElementForConnectedAnimation("animationKeyEvents", itemHero);
+            if (e.Parameter is long orderID)
+            {
+                await ViewModel.InitializeAsync(orderID);
+            }
+        }
+
+        protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            base.OnNavigatingFrom(e);
+            if (e.NavigationMode == NavigationMode.Back)
+            {
+                NavigationService.Frame.SetListDataItemForNextConnectedAnimation(ViewModel.Item);
+            }
+        }
+    }
+}
